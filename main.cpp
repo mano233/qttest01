692af5484f594b182dbc2d8502d9f2167d275197
likang <likang@moxigame.cn>
2021-09-17 18:41:34 +0800

    添加贝赛尔库

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQuickView>
+#include <iostream>
 #include "d3d11squircle.h"
+#include "lib/berizr.h"
+
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
@@ -11,7 +14,8 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
     qmlRegisterType<D3D11Squircle>("D3D11UnderQML", 1, 0, "D3D11Squircle");
     QQuickWindow::setGraphicsApi(QSGRendererInterface::Direct3D11);
-
+    Bezier::Bezier<3> b({{110,150},{25,190},{210,250},{210,30}});
+    std::cout<<b.length(3)<<std::endl;
     QQuickView view;
     view.setResizeMode(QQuickView::SizeRootObjectToView);
     view.setSource(QUrl("qrc:/main.qml"));
